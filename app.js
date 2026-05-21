2a0ff23d9a9e7a1125e3283c2170ad20d8c08b66
PrincesMargarethLatiza <PrincesMargarethLatiza>
2026-05-21 21:30:35 +0800

    Verify app.set('trust proxy', true) is set.

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ import cors from 'cors';
 import { connectDB } from './config/db.js';
 import notificationRoutes, { limiter as logsLimiter } from './routers/notificationRoutes.js';
 
+app.set('trust proxy', true);
 
 export default app;
